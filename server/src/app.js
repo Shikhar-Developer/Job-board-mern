8bdb36f9208a1f963e1fa5f8af59b5d09d28b98e
Shikhar Chaturvedi <shikhargithubid@gmail.com>
2026-07-14 18:08:25 +0530

    Installed a middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,12 @@
 import express from "express"
 
 const app = express()
+
+app.use((req, res, next) => {
+    console.log(`${req.method}, ${req.url}`);
+    next();
+});
+
 app.get("/", (req, res) => res.send("Server started!"))
 
 export default app
