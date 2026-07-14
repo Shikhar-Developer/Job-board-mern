43edbcce185f8d325633df87010d7a97135f66bb
Shikhar Chaturvedi <shikhargithubid@gmail.com>
2026-07-14 19:54:24 +0530

    Connected succesfully to moongoDB

---
+++ server/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connection Succesful! Connected to mongoDB")
+    } catch (error) {
+        console.error("Error!")
+        console.error(error.message)
+        process.exit(1)
+    }
+};
+
+export default connectDB;
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,12 @@
 import app from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"; // ./ -> current Directory / ../ -> Parent Directory 
 
-const PORT = 5000;
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => console.log(`Started Server Succesfully at http://localhost: ${PORT}`));
