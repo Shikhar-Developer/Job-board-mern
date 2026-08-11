bdf4ecf65e08683170f4ded408c10d22def8ed1f
Shikhar Chaturvedi <shikhargithubid@gmail.com>
2026-08-11 23:44:01 +0530

    App.js fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,38 @@
 // server/src/app.js
-import express from "express"
-import cors from "cors"
-import jobRouter from "./routes/jobs.routes.js"
-import authRouter from "./routes/auth.routes.js"
-import errorHandler from "./middleware/error.middleware.js"
-import applicationRouter from "./routes/application.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-import candidateRouter from "./routes/candidate.routes.js"
-import employerRouter from "./routes/employer.routes.js"
-import path from "path"
-import { fileURLToPath } from "url"
 
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
+import jobRouter from "./routes/jobs.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import errorHandler from "./middleware/error.middleware.js";
+import applicationRouter from "./routes/application.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import candidateRouter from "./routes/candidate.routes.js";
+import employerRouter from "./routes/employer.routes.js";
 
-const app = express()
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}))
-app.use(express.json())
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+);
+
+app.use(express.json());
+
+app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "../uploads"))
+);
 
 app.use((req, res, next) => {
     console.log(`${req.method}, ${req.url}`);
@@ -36,6 +45,7 @@ app.use("/api/application", applicationRouter);
 app.use("/api/dashboard", dashboardRouter);
 app.use("/api/candidate", candidateRouter);
 app.use("/api/employer", employerRouter);
+
 app.use(errorHandler);
 
 export default app;
